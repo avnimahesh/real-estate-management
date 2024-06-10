0ff71d2def56842689e671166e2244ee69ed7f51
Avni Maheshwari <54777288+avnimahesh@users.noreply.github.com>
2024-06-10 17:10:45 +0530

    Update cors.js

--- backend-fastify/src/cors.js
+++ backend-fastify/src/cors.js
@@ -6,7 +6,7 @@ export const setFastifyCors = function (fastify) {
     origin: [
       "http://localhost:9000",
       "http://localhost:8100",
-      "http://localhost:4200",
+      "http://54.67.78.130:4200",
     ],
   });
 };
