5d7b00c03b70a2360dfc6ca3819b708c663de4ab
Avni Maheshwari <54777288+avnimahesh@users.noreply.github.com>
2024-06-20 03:47:33 +0530

    Delete backend-fastify/routes directory

--- backend-fastify/routes/auth/options/index.js
+++
@@ -1,5 +0,0 @@
-import { registerOpts } from "./register.js";
-import { signInOpts } from "./signin.js";
-import { changePasswordOpts } from "./change-password.js";
-
-export { registerOpts, signInOpts, changePasswordOpts };
